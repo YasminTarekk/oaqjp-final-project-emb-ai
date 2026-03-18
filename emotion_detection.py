76cd827931eb16fe3de3ea23ed6492e5dde46938
YasminTarekk <yasmiintarek2003@gmail.com>
2026-03-18 11:11:14 -0400

    add emotion detetctor

--- emotion_detection.py
+++ emotion_detection.py
@@ -17,3 +17,14 @@ def emotion_detector(text_to_analyse):
     result = response.json()
 
     emotions = result["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion = max(emotions , key = emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
